ae8d01fa0c3a5077a89399abd4bf07a17f0b02fd
arpmipg <arpmipg@2546a24d-b155-0410-8cf7-15963867b5ce>
2009-04-05 15:05:55 +0000

    Issue 5

--- Tetris/Tetris/BlocksGrid.cs
+++ Tetris/Tetris/BlocksGrid.cs
@@ -1,203 +1,204 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
 using XnaTetris.Blocks;
 using XnaTetris.Game;
-using XnaTetris.Sounds;
-using XnaTetris.Graphics;
 using XnaTetris.Helpers;
-using Microsoft.Xna.Framework.Graphics;
 using XnaTetris.Algorithms;
 
 namespace XnaTetris
 {
-	class BlocksGrid : DrawableGameComponent
-	{
-		#region  онстанты
+  class BlocksGrid : DrawableGameComponent
+  {
+    #region  онстанты
 
-		public const int GridWidth = 8;
-		public const int GridHeight = 8;
+    public const int GridWidth = 8;
+    public const int GridHeight = 8;
 
-		public static readonly int NumOfBlockTypes = EnumHelper.GetSize(typeof(BlockFactory.BlockType));
+    public static readonly int NumOfBlockTypes = EnumHelper.GetSize(typeof(BlockFactory.BlockType));
 
-		#endregion
+    #endregion
 
-		#region Variables
+    #region Variables
 
+    public readonly LinesGame game;
+    private readonly Block[,] grid = new Block[GridWidth, GridHeight];
+    private readonly FindLines finder;
 
-		public readonly LinesGame game = null;
-		private readonly Block[,] grid = new Block[GridWidth, GridHeight];
-		private Rectangle gridRect;
-		private readonly FindLines finder;
+    private int _x1, _y1, _x2, _y2; //используютс€, чтобы отметить свапающиес€ блоки
 
-		private int _x1, _y1, _x2, _y2; //используютс€, чтобы отметить свапающиес€ блоки
+    #endregion
 
-		#endregion
+    #region Constructor
 
-		#region Constructor
+    public BlocksGrid(LinesGame setGame, Rectangle setGridRect)
+      : base(setGame)
+    {
+      game = setGame;
+      GridRect = setGridRect;
+      finder = new FindLines(this, GridWidth, GridHeight);
+    }
 
-		public BlocksGrid(LinesGame setGame, Rectangle setGridRect)
-			: base(setGame)
-		{
-			game = setGame;
-			GridRect = setGridRect;
-			finder = new FindLines(this, GridWidth, GridHeight);
-		}
+    #endregion
 
-		#endregion
+    #region Properties
 
-		#region Properties
+    public Block[,] Grid
+    {
+      get { return grid; }
+    }
 
-		public Block[,] Grid
-		{
-			get { return grid; }
-		}
+    public Rectangle GridRect { get; set; }
 
-		public Rectangle GridRect
-		{
-			get { return gridRect; }
-			set { gridRect = value; }
-		}
+    #endregion
 
-		#endregion
+    #region Initialize
 
-		#region Initialize
+    public override void Initialize()
+    {
+      Restart();
 
-		public override void Initialize()
-		{
-			Restart();
+      base.Initialize();
+    } // Initialize()
 
-			base.Initialize();
-		} // Initialize()
+    #endregion
 
-		#endregion
+    #region Restart
+    public void Restart()
+    {
+      for (int x = 0; x < GridWidth; x++)
+      {
+        for (int y = 0; y < GridHeight; y++)
+        {
+          // repeat until the board is in stable state
+          while (true)
+          {
+            AddNewRandomBlock(x, y);
 
-		#region Restart
-		public void Restart()
-		{
-			for (int x = 0; x < GridWidth; x++)
-				for (int y = 0; y < GridHeight; y++)
-				{
-					AddNewRandomBlock(x, y);
-				} // for for
-			finder.FindAndDestroyLines(new GameTime());
-			//Sound.Play(Sound.Sounds.Fight);
-		} // Restart()
-		#endregion
+            Block curBlock = Grid[x, y];
 
-		#region Update
-		public override void Update(GameTime gameTime)
-		{
-			if (!game.IsMoving && game.GameState == Serv.GameState.GameStateRunning)
-			{
-				if (Input.MouseLeftButtonJustPressed)
-				{
-					UpdateClickedBlock(Input.MousePos, gameTime);
-				}
-			}
-			foreach (Block block in Grid)
-			{
-				block.Update(gameTime);
-			}
-		} // Update()
+            if ((x < 2 || !curBlock.Type.Equals(Grid[x - 1, y].Type) || !curBlock.Type.Equals(Grid[x - 2, y].Type)) &&
+                (y < 2 || !curBlock.Type.Equals(Grid[x, y - 1].Type) || !curBlock.Type.Equals(Grid[x, y - 2].Type)))
+            {
+              break;
+            }
+          }
+        }
+      }
+      //Sound.Play(Sound.Sounds.Fight);
+    } // Restart()
+    #endregion
 
-		private void UpdateClickedBlock(Point point, GameTime gameTime)
-		{
-			foreach (Block block in Grid)
-			{
-				if (block.PointInBlock(Serv.CorrectPositionWithGameScale(point)))
-					{
-						int count = finder.ClickedBlocksCount();
-						if (count == 0)
-							block.ClickToBlock(gameTime); //TODO
-						else if (count == 1)
-						{
-							int xN, yN;
-							if (finder.NeighbourClickedBlock(block.X, block.Y, out xN, out yN))
-							{
-								block.ClickToBlock(gameTime);
-								_x1 = block.X;
-								_x2 = xN;
-								_y1 = block.Y;
-								_y2 = yN;
-								MoveBlocks(_x1, _y1, _x2, _y2, gameTime);
-							}
-							else
-								CleanClickedStates();
-						}
-						//else throw new Exception();
-					}
-				}
-		}//UpdateClickedBlock(Point, GameTime)
+    #region Update
+    public override void Update(GameTime gameTime)
+    {
+      if (!game.IsMoving && game.GameState == Serv.GameState.GameStateRunning)
+      {
+        if (Input.MouseLeftButtonJustPressed)
+        {
+          UpdateClickedBlock(Input.MousePos, gameTime);
+        }
+      }
+      foreach (Block block in Grid)
+      {
+        block.Update(gameTime);
+      }
+    } // Update()
 
-		#endregion
+    private void UpdateClickedBlock(Point point, GameTime gameTime)
+    {
+      foreach (Block block in Grid)
+      {
+        if (block.PointInBlock(Serv.CorrectPositionWithGameScale(point)))
+        {
+          int count = finder.ClickedBlocksCount();
+          if (count == 0)
+            block.ClickToBlock(gameTime); //TODO
+          else if (count == 1)
+          {
+            int xN, yN;
+            if (finder.NeighbourClickedBlock(block.X, block.Y, out xN, out yN))
+            {
+              block.ClickToBlock(gameTime);
+              _x1 = block.X;
+              _x2 = xN;
+              _y1 = block.Y;
+              _y2 = yN;
+              MoveBlocks(_x1, _y1, _x2, _y2, gameTime);
+            }
+            else
+              CleanClickedStates();
+          }
+          //else throw new Exception();
+        }
+      }
+    }//UpdateClickedBlock(Point, GameTime)
 
-		#region render
-		public override void Draw(GameTime gameTime)
-		{
-			for (int x = 0; x < GridWidth; x++)
-				for (int y = 0; y < GridHeight; y++)
-				{
-					Grid[x,y].Draw(gameTime);
-				}
-		}
-		#endregion
+    #endregion
 
-		#region functions
+    #region render
+    public override void Draw(GameTime gameTime)
+    {
+      for (int x = 0; x < GridWidth; x++)
+        for (int y = 0; y < GridHeight; y++)
+        {
+          Grid[x, y].Draw(gameTime);
+        }
+    }
+    #endregion
 
-		public void EndSwappingBlocks(GameTime gameTime)
-		{
-			finder.SwapBlocks(_x1, _y1, _x2, _y2);
-			finder.FindAndDestroyLines(gameTime);
-			CleanClickedStates();
-		}
+    #region functions
 
-		private void MoveBlocks(int x, int y, int xN, int yN, GameTime gameTime)
-		{
-			Grid[x, y].MakeMove(gameTime, finder.GetRectangle(xN, yN), xN, yN, 0);
-			Grid[xN, yN].MakeMove(gameTime, finder.GetRectangle(x, y), x, y, 0);
-		}
+    public void EndSwappingBlocks(GameTime gameTime)
+    {
+      finder.SwapBlocks(_x1, _y1, _x2, _y2);
+      finder.FindAndDestroyLines(gameTime);
+      CleanClickedStates();
+    }
 
-		private void CleanClickedStates()
-		{
-			for (int x = 0; x < GridWidth; x++)
-				for (int y = 0; y < GridHeight; y++)
-					Grid[x, y].IsClicked = false;
-		}//CleanClickedStates()
+    private void MoveBlocks(int x, int y, int xN, int yN, GameTime gameTime)
+    {
+      Grid[x, y].MakeMove(gameTime, finder.GetRectangle(xN, yN), xN, yN, 0);
+      Grid[xN, yN].MakeMove(gameTime, finder.GetRectangle(x, y), x, y, 0);
+    }
 
-		public void AddNewRandomBlock(int x, int y)
-		{
-			Grid[x, y] = (BlockFactory.GetBlockFactory(game).GetNewBlock(BlockFactory.GetNewBlockType(),
-				finder.GetRectangle(x, y), x, y));
-			Grid[x, y].EndMove += BlocksGrid_EndMove;
-		}
+    private void CleanClickedStates()
+    {
+      for (int x = 0; x < GridWidth; x++)
+        for (int y = 0; y < GridHeight; y++)
+          Grid[x, y].IsClicked = false;
+    }//CleanClickedStates()
 
-		public void AddNewRandomBlock(int x, int y, BlockFactory.BlockType oldType, int luck)
-		{
-			Grid[x, y] = (BlockFactory.GetBlockFactory(game).GetNewBlock(
-				RandomBlockHelper.GenerateNewBlockType(oldType, luck),
-				finder.GetRectangle(x, y), x, y));
-			Grid[x, y].EndMove += BlocksGrid_EndMove;
-		}
+    public void AddNewRandomBlock(int x, int y)
+    {
+      Grid[x, y] = (BlockFactory.GetBlockFactory(game).GetNewBlock(BlockFactory.GetRandomBlockType(),
+        finder.GetRectangle(x, y), x, y));
+      Grid[x, y].EndMove += BlocksGrid_EndMove;
+    }
 
-		// если просто закончили движение - сдвигаем блок, если свапались - то вызываем EndSwappingBlocks,
-		// второй свапающийс€ блок не вызовет EndSwappingBlocks, так как в первом все Clicked подчист€тс€
-		void BlocksGrid_EndMove(object sender, EventArgs e)
-		{
-			if (!(sender is Block))
-				return;
-			Block temp = sender as Block;
-			if (temp.IsClicked)
-				EndSwappingBlocks(temp.blockGameTime);
-			else
-			{
-				temp.BlockRectangle = finder.GetRectangle(temp.X, temp.Y);
-				Grid[temp.X, temp.Y] = temp;
-			}
-		}
+    public void AddNewRandomBlock(int x, int y, BlockFactory.BlockType oldType, int luck)
+    {
+      Grid[x, y] = (BlockFactory.GetBlockFactory(game).GetNewBlock(
+        RandomBlockHelper.GenerateNewBlockType(oldType, luck),
+        finder.GetRectangle(x, y), x, y));
+      Grid[x, y].EndMove += BlocksGrid_EndMove;
+    }
 
-		#endregion
-	}
+    // если просто закончили движение - сдвигаем блок, если свапались - то вызываем EndSwappingBlocks,
+    // второй свапающийс€ блок не вызовет EndSwappingBlocks, так как в первом все Clicked подчист€тс€
+    void BlocksGrid_EndMove(object sender, EventArgs e)
+    {
+      if (!(sender is Block))
+        return;
+      Block temp = sender as Block;
+      if (temp.IsClicked)
+        EndSwappingBlocks(temp.blockGameTime);
+      else
+      {
+        temp.BlockRectangle = finder.GetRectangle(temp.X, temp.Y);
+        Grid[temp.X, temp.Y] = temp;
+      }
+    }
+
+    #endregion
+  }
 }
