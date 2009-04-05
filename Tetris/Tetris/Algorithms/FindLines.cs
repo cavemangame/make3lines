e4ce6e61021deacb544ca01ecc6b301d584ecbe7
arpmipg <arpmipg@2546a24d-b155-0410-8cf7-15963867b5ce>
2009-04-05 15:49:16 +0000

    refactoring

--- Tetris/Tetris/Algorithms/FindLines.cs
+++ Tetris/Tetris/Algorithms/FindLines.cs
@@ -1,215 +1,218 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
 using Microsoft.Xna.Framework;
 using XnaTetris.Blocks;
 using XnaTetris.Game;
 
 namespace XnaTetris.Algorithms
 {
-	class FindLines
-	{
-		#region Variables
+  class FindLines
+  {
+    #region Variables
 
-		private readonly int GridHeight;
-		private readonly int GridWidth;
-		private readonly BlocksGrid bGrid;
+    private readonly int GridHeight;
+    private readonly int GridWidth;
+    private readonly BlocksGrid bGrid;
 
-		#endregion
+    #endregion
 
-		#region Constructor
-		
-		public FindLines(BlocksGrid setGrid, int GridWidth, int GridHeight)
-		{
-			bGrid = setGrid;
-			this.GridWidth = GridWidth;
-			this.GridHeight = GridHeight;
-		}
+    #region Constructor
 
-		#endregion
+    public FindLines(BlocksGrid setGrid, int GridWidth, int GridHeight)
+    {
+      bGrid = setGrid;
+      this.GridWidth = GridWidth;
+      this.GridHeight = GridHeight;
+    }
 
-		#region Finder funcrions
+    #endregion
 
-		private bool IsSameTypeNHorizBlock(int y, int x, int n)
-		{
-			Type type = bGrid.Grid[x, y].GetType();
-			for (int i = 1; i < n; i++)
-			{
-				if (bGrid.Grid[x + i, y].IsDestroyed)
-					return false;
-				if (type != bGrid.Grid[x + i, y].GetType())
-					return false;
-			}
-			return true;
-		}
+    #region Finder funcrions
 
-		private bool IsSameTypeNVertBlock(int x, int y, int n)
-		{
-			Type type = bGrid.Grid[x, y].GetType();
-			for (int i = 1; i < n; i++)
-			{
-				if (bGrid.Grid[x, y + i].IsDestroyed)
-					return false;
-				if (type != bGrid.Grid[x, y + i].GetType())
-					return false;
-			}
-			return true;
-		}
+    private bool IsSameTypeNHorizBlock(int y, int x, int n)
+    {
+      Block block = bGrid.Grid[x, y];
 
-		public bool NeighbourClickedBlock(int xx, int yy, out int xN, out int yN)
-		{
-			if (xx > 0 && bGrid.Grid[xx - 1, yy].IsClicked)
-			{
-				xN = xx - 1;
-				yN = yy;
-				return true;
-			}
-			if (xx < GridWidth - 1 && bGrid.Grid[xx + 1, yy].IsClicked)
-			{
-				xN = xx + 1;
-				yN = yy;
-				return true;
-			}
-			if (yy > 0 && bGrid.Grid[xx, yy - 1].IsClicked)
-			{
-				xN = xx;
-				yN = yy - 1;
-				return true;
-			}
-			if (yy < GridHeight - 1 && bGrid.Grid[xx, yy + 1].IsClicked)
-			{
-				xN = xx;
-				yN = yy + 1;
-				return true;
-			}
-			xN = yN = -1;
-			return false;
-		}//NeighbourClickedBlock(int,int)
+      for (int i = 1; i < n; i++)
+      {
+        Block curBlock = bGrid.Grid[x + i, y];
 
-		private bool FindNLines(int N)
-		{
-			bool result = false;
-			// сначала по горизонтали
-			for (int y = 0; y < GridHeight; y++)
-			{
-				for (int x = 0; x < GridWidth - (N-1); x++)
-				{
-					if (IsSameTypeNHorizBlock(y, x, N))
-					{
-						result = true;
-						for (int xx = x; xx < x + N; xx++)
-							bGrid.Grid[xx, y].IsDestroyed = true;
+        if (curBlock.IsDestroyed || !block.Type.Equals(curBlock.Type))
+        {
+          return false;
+        }
+      }
+      return true;
+    }
 
-						bGrid.game.Score += bGrid.Grid[x, y].GetScore(N);
-					}
-				}
-			}
+    private bool IsSameTypeNVertBlock(int x, int y, int n)
+    {
+      Block block = bGrid.Grid[x, y];
 
-			// по вертикали
-			for (int x = 0; x < GridWidth; x++)
-			{
-				for (int y = 0; y < GridHeight - (N - 1); y++)
-				{
-					if (IsSameTypeNVertBlock(x, y, N))
-					{
-						result = true;
-						for (int yy = y; yy < y + N; yy++)
-							bGrid.Grid[x, yy].IsDestroyed = true;
-						bGrid.game.Score += bGrid.Grid[x, y].GetScore(N);
-					}
-				}
-			}
-			return result;
-		}
+      for (int i = 1; i < n; i++)
+      {
+        Block curBlock = bGrid.Grid[x, y + i];
 
-		#endregion
+        if (curBlock.IsDestroyed || !block.Type.Equals(curBlock.Type))
+        {
+          return false;
+        }
+      }
+      return true;
+    }
 
-		#region Destroy Blocks
+    public bool NeighbourClickedBlock(int xx, int yy, out int xN, out int yN)
+    {
+      if (xx > 0 && bGrid.Grid[xx - 1, yy].IsClicked)
+      {
+        xN = xx - 1;
+        yN = yy;
+        return true;
+      }
+      if (xx < GridWidth - 1 && bGrid.Grid[xx + 1, yy].IsClicked)
+      {
+        xN = xx + 1;
+        yN = yy;
+        return true;
+      }
+      if (yy > 0 && bGrid.Grid[xx, yy - 1].IsClicked)
+      {
+        xN = xx;
+        yN = yy - 1;
+        return true;
+      }
+      if (yy < GridHeight - 1 && bGrid.Grid[xx, yy + 1].IsClicked)
+      {
+        xN = xx;
+        yN = yy + 1;
+        return true;
+      }
+      xN = yN = -1;
+      return false;
+    }//NeighbourClickedBlock(int,int)
 
-		public void FindAndDestroyLines(GameTime gameTime)
-		{
-			bool result = FindNLines(5) | FindNLines(4) | FindNLines(3);
+    private bool FindNLines(int N)
+    {
+      bool result = false;
+      // сначала по горизонтали
+      for (int y = 0; y < GridHeight; y++)
+      {
+        for (int x = 0; x < GridWidth - (N - 1); x++)
+        {
+          if (IsSameTypeNHorizBlock(y, x, N))
+          {
+            result = true;
+            for (int xx = x; xx < x + N; xx++)
+              bGrid.Grid[xx, y].IsDestroyed = true;
 
-			RemoveLines(gameTime);
-			if (result)
-				FindAndDestroyLines(gameTime);
-		}
+            bGrid.game.Score += bGrid.Grid[x, y].GetScore(N);
+          }
+        }
+      }
 
-		private void RemoveLines(GameTime gameTime)
-		{
-			for (int j = GridHeight - 1; j >= 0; j--)
-				RemoveHorizLine(gameTime, j);
-		}
+      // по вертикали
+      for (int x = 0; x < GridWidth; x++)
+      {
+        for (int y = 0; y < GridHeight - (N - 1); y++)
+        {
+          if (IsSameTypeNVertBlock(x, y, N))
+          {
+            result = true;
+            for (int yy = y; yy < y + N; yy++)
+              bGrid.Grid[x, yy].IsDestroyed = true;
+            bGrid.game.Score += bGrid.Grid[x, y].GetScore(N);
+          }
+        }
+      }
+      return result;
+    }
 
-		private void RemoveHorizLine(GameTime gameTime, int j)
-		{
-			// подчищаем удаленные блоки с одновременным спуском столбцов, до тех пор, пока на линии останутся только
-			// неудаленные
-			bool flag = false;
-			for (int i = 0; i < GridWidth; i++)
-			{
-				if (bGrid.Grid[i, j].IsDestroyed)
-				{
-					DownBlockColumn(gameTime, i, j);
-					flag = true;
-				}
-			}
-			if (flag) RemoveHorizLine(gameTime, j);
-		}
+    #endregion
 
-		private void DownBlockColumn(GameTime gameTime, int i, int jj)
-		{
-			if (jj > 0)
-			{
-				bGrid.Grid[i, jj].X = bGrid.Grid[i, jj].Y = -1;
-				bGrid.Grid[i, jj].BlockRectangle = Serv.EmptyRect;
+    #region Destroy Blocks
 
-				for (int j = jj - 1; j >= 0; j--)
-				{
-					bGrid.Grid[i, j + 1] = bGrid.Grid[i, j];
-					bGrid.Grid[i, j].MakeMove(gameTime, GetRectangle(i, j + 1), i, j + 1, 0);
-				}
-			}
-			// наверху сразу генерим новый
-			bGrid.AddNewRandomBlock(i, 0, bGrid.Grid[i, jj].Type, 2);
-		}
+    public void FindAndDestroyLines(GameTime gameTime)
+    {
+      bool result = FindNLines(5) | FindNLines(4) | FindNLines(3);
 
-		public void SwapBlocks(int x1, int y1, int x2, int y2)
-		{
-			Block temp = bGrid.Grid[x1, y1];
-			bGrid.Grid[x1, y1] = bGrid.Grid[x2, y2];
-			bGrid.Grid[x2, y2] = temp;
+      RemoveLines(gameTime);
+      if (result)
+        FindAndDestroyLines(gameTime);
+    }
 
-			bGrid.Grid[x1, y1].BlockRectangle = GetRectangle(x1, y1);
-			bGrid.Grid[x2, y2].BlockRectangle = GetRectangle(x2, y2);
+    private void RemoveLines(GameTime gameTime)
+    {
+      for (int j = GridHeight - 1; j >= 0; j--)
+        RemoveHorizLine(gameTime, j);
+    }
 
-			bGrid.Grid[x1, y1].IsClicked = bGrid.Grid[x2, y2].IsClicked = false;
-		}//LazySwapBlocks(Block, Block)
+    private void RemoveHorizLine(GameTime gameTime, int j)
+    {
+      // подчищаем удаленные блоки с одновременным спуском столбцов, до тех пор, пока на линии останутся только
+      // неудаленные
+      bool flag = false;
+      for (int i = 0; i < GridWidth; i++)
+      {
+        if (bGrid.Grid[i, j].IsDestroyed)
+        {
+          DownBlockColumn(gameTime, i, j);
+          flag = true;
+        }
+      }
+      if (flag) RemoveHorizLine(gameTime, j);
+    }
 
-		#endregion
+    private void DownBlockColumn(GameTime gameTime, int i, int jj)
+    {
+      if (jj > 0)
+      {
+        bGrid.Grid[i, jj].X = bGrid.Grid[i, jj].Y = -1;
+        bGrid.Grid[i, jj].BlockRectangle = Serv.EmptyRect;
 
-		#region Some functions
+        for (int j = jj - 1; j >= 0; j--)
+        {
+          bGrid.Grid[i, j + 1] = bGrid.Grid[i, j];
+          bGrid.Grid[i, j].MakeMove(gameTime, GetRectangle(i, j + 1), i, j + 1, 0);
+        }
+      }
+      // наверху сразу генерим новый
+      bGrid.AddNewRandomBlock(i, 0, bGrid.Grid[i, jj].Type, 2);
+    }
 
-		public Rectangle GetRectangle(int x, int y)
-		{
-			return new Rectangle(bGrid.GridRect.X + x * bGrid.GridRect.Width / GridWidth,
-													 bGrid.GridRect.Y + y * bGrid.GridRect.Height / GridHeight,
-													 bGrid.GridRect.Width / GridWidth,
-													 bGrid.GridRect.Height / GridHeight);
-		}//GetRectangle(int, int)
+    public void SwapBlocks(int x1, int y1, int x2, int y2)
+    {
+      Block temp = bGrid.Grid[x1, y1];
+      bGrid.Grid[x1, y1] = bGrid.Grid[x2, y2];
+      bGrid.Grid[x2, y2] = temp;
 
-		public int ClickedBlocksCount()
-		{
-			int count = 0;
-			for (int x = 0; x < GridWidth; x++)
-				for (int y = 0; y < GridHeight; y++)
-				{
-					if (bGrid.Grid[x, y].IsClicked)
-						count++;
-				}
-			return count;
-		}//ClickedBlocksCount()
+      bGrid.Grid[x1, y1].BlockRectangle = GetRectangle(x1, y1);
+      bGrid.Grid[x2, y2].BlockRectangle = GetRectangle(x2, y2);
 
-		#endregion
-	}
+      bGrid.Grid[x1, y1].IsClicked = bGrid.Grid[x2, y2].IsClicked = false;
+    }//LazySwapBlocks(Block, Block)
+
+    #endregion
+
+    #region Some functions
+
+    public Rectangle GetRectangle(int x, int y)
+    {
+      return new Rectangle(bGrid.GridRect.X + x * bGrid.GridRect.Width / GridWidth,
+                           bGrid.GridRect.Y + y * bGrid.GridRect.Height / GridHeight,
+                           bGrid.GridRect.Width / GridWidth,
+                           bGrid.GridRect.Height / GridHeight);
+    }//GetRectangle(int, int)
+
+    public int ClickedBlocksCount()
+    {
+      int count = 0;
+      for (int x = 0; x < GridWidth; x++)
+        for (int y = 0; y < GridHeight; y++)
+        {
+          if (bGrid.Grid[x, y].IsClicked)
+            count++;
+        }
+      return count;
+    }//ClickedBlocksCount()
+
+    #endregion
+  }
 }
