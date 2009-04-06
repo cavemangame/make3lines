b950d976c699371504ecee7c8e04bc0add9d0da6
acid-jungle@yandex.ru <acid-jungle@yandex.ru@2546a24d-b155-0410-8cf7-15963867b5ce>
2009-04-06 17:45:26 +0000

    Рефакторинг: перенос загрузки ресурсов меню в класс меню, Спрайты блоков теперь в ContentSpace

--- trunk/Tetris/Tetris/Blocks/BlockFactory.cs
+++ trunk/Tetris/Tetris/Blocks/BlockFactory.cs
@@ -1,13 +1,13 @@
 using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using XnaTetris.Game;
 using XnaTetris.Helpers;
 
 namespace XnaTetris.Blocks
 {
   class BlockFactory
   {
-    private SpriteHelper greenBlock, redBlock, blueBlock, yellowBlock, blackBlock, someBlock;
     private readonly LinesGame game;
 
     private static BlockFactory instance;
@@ -35,12 +35,12 @@ namespace XnaTetris.Blocks
         game.Content.RootDirectory = "Content";
 
         // Create block sprites
-        greenBlock = new SpriteHelper(game.Content.Load<Texture2D>("BlocksGeneral"), new Rectangle(0, 0, 64, 64));
-        redBlock = new SpriteHelper(game.Content.Load<Texture2D>("BlocksGeneral"), new Rectangle(64, 0, 64, 64));
-        blueBlock = new SpriteHelper(game.Content.Load<Texture2D>("BlocksGeneral"), new Rectangle(64*2, 0, 64, 64));
-        yellowBlock = new SpriteHelper(game.Content.Load<Texture2D>("BlocksGeneral"), new Rectangle(64*3, 0, 64, 64));
-        blackBlock = new SpriteHelper(game.Content.Load<Texture2D>("BlocksGeneral"), new Rectangle(64*4, 0, 64, 64));
-        someBlock = new SpriteHelper(game.Content.Load<Texture2D>("BlocksGeneral"), new Rectangle(64*5, 0, 64, 64));
+        ContentSpace.greenBlock = new SpriteHelper(game.Content.Load<Texture2D>("BlocksGeneral"), new Rectangle(0, 0, 64, 64));
+        ContentSpace.redBlock = new SpriteHelper(game.Content.Load<Texture2D>("BlocksGeneral"), new Rectangle(64, 0, 64, 64));
+        ContentSpace.blueBlock = new SpriteHelper(game.Content.Load<Texture2D>("BlocksGeneral"), new Rectangle(64 * 2, 0, 64, 64));
+        ContentSpace.yellowBlock = new SpriteHelper(game.Content.Load<Texture2D>("BlocksGeneral"), new Rectangle(64 * 3, 0, 64, 64));
+        ContentSpace.blackBlock = new SpriteHelper(game.Content.Load<Texture2D>("BlocksGeneral"), new Rectangle(64 * 4, 0, 64, 64));
+        ContentSpace.someBlock = new SpriteHelper(game.Content.Load<Texture2D>("BlocksGeneral"), new Rectangle(64 * 5, 0, 64, 64));
       }
     }
 
@@ -58,17 +58,17 @@ namespace XnaTetris.Blocks
       switch (blockType)
       {
         case BlockType.Green:
-          return new BlockGreen(game, setRect, greenBlock, x, y);
+          return new BlockGreen(game, setRect, ContentSpace.greenBlock, x, y);
         case BlockType.Red:
-          return new BlockRed(game, setRect, redBlock, x, y);
+          return new BlockRed(game, setRect, ContentSpace.redBlock, x, y);
         case BlockType.Blue:
-          return new BlockBlue(game, setRect, blueBlock, x, y);
+          return new BlockBlue(game, setRect, ContentSpace.blueBlock, x, y);
         case BlockType.Yellow:
-          return new BlockYellow(game, setRect, yellowBlock, x, y);
+          return new BlockYellow(game, setRect, ContentSpace.yellowBlock, x, y);
         case BlockType.Black:
-          return new BlockBlack(game, setRect, blackBlock, x, y);
+          return new BlockBlack(game, setRect, ContentSpace.blackBlock, x, y);
         case BlockType.Someone:
-          return new BlockSomeone(game, setRect, someBlock, x, y);
+          return new BlockSomeone(game, setRect, ContentSpace.someBlock, x, y);
         default:
           throw new ArgumentOutOfRangeException("blockType");
       }
