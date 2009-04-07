3d2815e5751683a14bf63cada086ae2bb4c85273
arpmipg <arpmipg@2546a24d-b155-0410-8cf7-15963867b5ce>
2009-04-07 14:04:32 +0000

    refactoring

--- Tetris/Tetris/Helpers/EnumHelper.cs
+++ Tetris/Tetris/Helpers/EnumHelper.cs
@@ -1,165 +1,120 @@
-// Project: XnaTetris, File: EnumHelper.cs
-// Namespace: XnaTetris.Helpers, Class: EnumHelper
-// Path: C:\code\XnaTetris\Helpers, Author: Abi
-// Code lines: 11, Size of file: 161 Bytes
-// Creation date: 24.11.2005 12:46
-// Last modified: 02.12.2005 19:33
-// Generated with Commenter by abi.exDream.com
-
 #region Using directives
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Collections;
 #endregion
 
 namespace XnaTetris.Helpers
 {
 #if !XBOX360
-	/// <summary>
-	/// Enum helper
-	/// </summary>
-	class EnumHelper
-	{
-		#region Enum enumerator class
-		/// <summary>
-		/// Enum enumerator helper for GetEnumerator,
-		/// this allow us to enumerate enums just like collections.
-		/// </summary>
-		public class EnumEnumerator : IEnumerator, IEnumerable
-		{
-			/// <summary>
-			/// The enum we use
-			/// </summary>
-			public System.Type enumType;
-			/// <summary>
-			/// Own index
-			/// </summary>
-			public int index;
-			/// <summary>
-			/// Length of enum
-			/// </summary>
-			public int enumLength;
+  /// <summary>
+  /// Enum helper
+  /// </summary>
+  class EnumHelper
+  {
+    #region Enum enumerator class
+    /// <summary>
+    /// Enum enumerator helper for GetEnumerator,
+    /// this allow us to enumerate enums just like collections.
+    /// </summary>
+    public class EnumEnumerator : IEnumerator, IEnumerable
+    {
+      public Type enumType;
+      public int index;
+      public int enumLength;
 
-			/// <summary>
-			/// Create enum enumerator
-			/// </summary>
-			public EnumEnumerator(System.Type setEnumType)
-			{
-				enumType = setEnumType;
-				index = -1;
-				enumLength = GetSize(enumType);
-			} // EnumEnumerator(setEnumType)
+      public EnumEnumerator(Type setEnumType)
+      {
+        enumType = setEnumType;
+        index = -1;
+        enumLength = GetSize(enumType);
+      }
 
-			/// <summary>
-			/// Own
-			/// </summary>
-			public object Current
-			{
-				get
-				{
-					if (index >= 0 &&
-						index < enumLength)
-						return Enum.GetValues(enumType).GetValue(new int[] { index });
-					else
-						// Just return first entry if index is invalid
-						return Enum.GetValues(enumType).GetValue(new int[] { 0 });
-				} // get
-			} // Current
+      public object Current
+      {
+        get
+        {
+          if (index >= 0 &&
+            index < enumLength)
+            return Enum.GetValues(enumType).GetValue(new[] { index });
+          // Just return first entry if index is invalid
+          return Enum.GetValues(enumType).GetValue(new[] { 0 });
+        }
+      }
 
-			/// <summary>
-			/// Move next
-			/// </summary>
-			public bool MoveNext()
-			{
-				index++;
+      public bool MoveNext()
+      {
+        index++;
 
-				// Finished?
-				return index < enumLength;
-			} // MoveNext()
+        return index < enumLength;
+      }
 
-			/// <summary>
-			/// Reset
-			/// </summary>
-			public void Reset()
-			{
-				index = -1;
-			} // Reset()
+      public void Reset()
+      {
+        index = -1;
+      }
 
-			/// <summary>
-			/// Get enumerator
-			/// </summary>
-			public IEnumerator GetEnumerator()
-			{
-				return this;
-			} // GetEnumerator()
-		} // class EnumEnumerator
-		#endregion
+      public IEnumerator GetEnumerator()
+      {
+        return this;
+      }
+    }
+    #endregion
 
-		#region Get size
-		/// <summary>
-		/// Get number of elements of a enum (accessing enum by type)
-		/// </summary>
-		public static int GetSize(System.Type enumType)
-		{
-			return Enum.GetNames(enumType).Length;
-		} // GetSize(enumType)
-		#endregion
+    #region Get size
+    /// <summary>
+    /// Get number of elements of a enum (accessing enum by type)
+    /// </summary>
+    public static int GetSize(Type enumType)
+    {
+      return Enum.GetNames(enumType).Length;
+    }
+    #endregion
 
-		#region Get enumerator
-		/// <summary>
-		/// Get enumerator
-		/// </summary>
-		public static EnumEnumerator GetEnumerator(System.Type enumType)
-		{
-			return new EnumEnumerator(enumType);
-		} // GetEnumerator(enumType)
-		#endregion
+    #region Get enumerator
+    public static EnumEnumerator GetEnumerator(Type enumType)
+    {
+      return new EnumEnumerator(enumType);
+    }
+    #endregion
 
-		#region Search enumerator
-		/// <summary>
-		/// Search enumerator
-		/// </summary>
-		/// <param name="type">Type</param>
-		/// <param name="name">Name</param>
-		/// <returns>Object</returns>
-		public static object SearchEnumerator(Type type, string name)
-		{
-			foreach (object objEnum in GetEnumerator(type))
-				if (StringHelper.Compare(objEnum.ToString(), name))
-					return objEnum;
+    #region Search enumerator
+    public static object SearchEnumerator(Type type, string name)
+    {
+      foreach (object objEnum in GetEnumerator(type))
+        if (StringHelper.Compare(objEnum.ToString(), name))
+          return objEnum;
 
-			// Else not found, just return first!
-			return 0;
-		} // SearchEnumerator(type, name)
-		#endregion
+      // Else not found, just return first!
+      return 0;
+    }
+    #endregion
 
-		#region Get all enum names
-		/// <summary>
-		/// Get all names from an enum.
-		/// E.g. If we have an enum with 3 values (A, B and C),
-		/// then return "A, B, C".
-		/// </summary>
-		/// <param name="type">Enum type, will be passed to
-		/// GetEnumerator</param>
-		/// <returns>String with all enum names</returns>
-		public static string GetAllEnumNames(Type type)
-		{
-			// Simplified version
-			return StringHelper.WriteArrayData(GetEnumerator(type));
+    #region Get all enum names
+    /// <summary>
+    /// Get all names from an enum.
+    /// E.g. If we have an enum with 3 values (A, B and C),
+    /// then return "A, B, C".
+    /// </summary>
+    /// <param name="type">Enum type, will be passed to
+    /// GetEnumerator</param>
+    /// <returns>String with all enum names</returns>
+    public static string GetAllEnumNames(Type type)
+    {
+      // Simplified version
+      return StringHelper.WriteArrayData(GetEnumerator(type));
 
-			/*same code, but more complicated:
-			List<string> returnList = new List<string>();
-			foreach (Enum enumValue in GetEnumerator(type))
-				returnList.Add(enumValue.ToString());
-			return StringHelper.WriteArrayData(returnList);
-			 */
-		} // GetAllEnumNames(type)
-		#endregion
+      /*same code, but more complicated:
+      List<string> returnList = new List<string>();
+      foreach (Enum enumValue in GetEnumerator(type))
+        returnList.Add(enumValue.ToString());
+      return StringHelper.WriteArrayData(returnList);
+       */
+    }
+    #endregion
 
-		#region Unit Testing
+    #region Unit Testing
 #if DEBUG
-		/*include NUnit.Framework for this
+    /*include NUnit.Framework for this
 		/// <summary>
 		/// EnumHelper tests to find out if all methods work properly.
 		/// </summary>
@@ -180,7 +135,7 @@ namespace XnaTetris.Helpers
 		} // class EnumHelperTests
 		 */
 #endif
-		#endregion
-	} // class EnumHelper
+    #endregion
+  }
 #endif
-} // namespace XnaTetris.Helpers
+}
