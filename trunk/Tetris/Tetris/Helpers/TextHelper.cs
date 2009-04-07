0f478a216538ff7f22a72eda7260872bdb159454
acid-jungle@yandex.ru <acid-jungle@yandex.ru@2546a24d-b155-0410-8cf7-15963867b5ce>
2009-04-07 20:49:10 +0000

    Issue 12: TextHelper finished

--- trunk/Tetris/Tetris/Helpers/TextHelper.cs
+++ trunk/Tetris/Tetris/Helpers/TextHelper.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace XnaTetris.Helpers
@@ -17,27 +13,23 @@ namespace XnaTetris.Helpers
       public int X { get; private set;} 
       public int Y { get; private set;}
       public Color TextColor { get; private set; }
+      public float Scale { get; private set; }
 
-      public TextToRender(SpriteFont font, string text, int x, int y, Color color)
+      public TextToRender(SpriteFont font, string text, int x, int y, Color color, float scale)
       {
         Font = font;
         Text = text;
         X = x;
         Y = y;
         TextColor = color;
-      }
-
-      public TextToRender(SpriteFont font, string text, int x, int y)
-        : this(font, text, x, y, Color.White)
-      {
-
+        Scale = scale;
       }
     }
 
     #region Variables
 
     private static SpriteBatch fontSprite;
-    private static List<TextToRender> renderTexts = new List<TextToRender>();
+    private static readonly List<TextToRender> renderTexts = new List<TextToRender>();
 
     #endregion
 
@@ -54,7 +46,18 @@ namespace XnaTetris.Helpers
 
     public static void DrawText(SpriteFont font, string text, int x, int y)
     {
-      renderTexts.Add(new TextToRender(font, text, x, y));
+      DrawText(font, text, x, y, Color.White);
+    }
+
+    public static void DrawText(SpriteFont font, string text, int x, int y, Color color)
+    {
+      DrawText(font, text, x, y, color, 1f);
+    }
+
+    // хер проссыт как из SpriteFont добть размер, поэтому метод со scale
+    public static void DrawText(SpriteFont font, string text, int x, int y, Color color, float scale)
+    {
+      renderTexts.Add(new TextToRender(font, text, x, y, color, scale));
     }
 
     public void WriteAll()
@@ -62,15 +65,14 @@ namespace XnaTetris.Helpers
       if (renderTexts.Count == 0)
         return;
 
-      // Start rendering
       fontSprite.Begin(SpriteBlendMode.AlphaBlend);
 
       foreach (TextToRender rt in renderTexts)
       {
-        fontSprite.DrawString(rt.Font, rt.Text, new Vector2(rt.X, rt.Y), rt.TextColor);
+        fontSprite.DrawString(rt.Font, rt.Text, new Vector2(rt.X, rt.Y), rt.TextColor, 0f,
+          new Vector2(0, 0), rt.Scale, SpriteEffects.None, 0);
       }
 
-      // End rendering
       fontSprite.End();
 
       renderTexts.Clear();
